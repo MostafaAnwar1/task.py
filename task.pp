af4dfe0266ce68a74dd3252889a758b21d9c10d5
MostafaAnwar1 <sasaanwar521@gmail.com>
2024-02-27 01:23:22 +0200

    solve the second task

---
+++ task.pp
@@ -0,0 +1,104 @@
+Python 3.11.8 (tags/v3.11.8:db85d51, Feb  6 2024, 21:52:07) [MSC v.1937 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 5==6
+x
+False
+y = 6
+y
+6
+
+z=10.5
+z
+10.5
+s = 'sasa'
+s
+'sasa'
+a = float (y)
+
+a
+6.0
+q = [1,2,3,4,5,]
+q
+[1, 2, 3, 4, 5]
+e = (10,11,12,13,14,15)
+e
+(10, 11, 12, 13, 14, 15)
+d = tuple (q)
+d
+(1, 2, 3, 4, 5)
+c = {1,2,3,}
+c
+{1, 2, 3}
+x = 10
+x
+10
+x = x+15
+x
+25
+x = x//5
+x
+5
+x = x*10
+x
+50
+x = x%3
+x
+2
+print (11%4)
+3
+r = 2**3
+r
+8
+b = 11/3
+b
+3.6666666666666665
+if 10 > 15:
+    print("10 is bigger than 15")
+else:
+    print("10 is not bigger than 15")
+... 
+...     
+10 is not bigger than 15
+>>> if 10 <= 15:
+...     print( "x is smaller than 15")
+... 
+x is smaller than 15
+>>> list = [2,4,6,8,10,]
+>>> list
+[2, 4, 6, 8, 10]
+>>> if 4 in list:
+...     print("4 is in the list.")
+... else:
+...     print("4 is not in the list.")
+... 
+... 
+4 is in the list.
+>>>  if 4 and 6 in list:
+...     print("4 and 6 is in the list.")
+... else:
+...     print("4 and 6 is  not in the list.")
+... 
+... 
+SyntaxError: unexpected indent
+>>> KeyboardInterrupt
+>>> if 4 and 6 in list:
+...     print("4 and 6 is in the list.")
+... else:
+...     print("4 and 6 is  not in the list.")
+... 
+...     
+4 and 6 is in the list.
+>>> if 3 and 6 in list:
+...     print("3 and 6 is in the list.")
+... else:
+...     print("3 and 6 is  not in the list.")
+... 
+...     
+3 and 6 is in the list.
+>>> if 2 and 4 and 5 in list:
+...     print("2 and 4 and 5 is in the list.")
+... else:
+...     print( "2 and 4 and 5 is  not in the list.")
+... 
+...     
+2 and 4 and 5 is  not in the list.
